48428cc1cfe3aa1abcc0612fef8348d1d5666787
Elena Terehovskaya <len-sh@yandex.ru>
2020-06-07 15:21:15 +0300

    Исправлено соотношение координат и адреса объявления

--- js/main.js
+++ js/main.js
@@ -15,6 +15,11 @@ var OBJECT_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var OBJECT_TIMES = ['12:00', '13:00', '14:00'];
 var OBJECT_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var OBJECT_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
 /**
  * Получает случайное число из диапазона
@@ -39,13 +44,15 @@ var getRandomItem = function (array) {
 var advertsData = [];
 
 for (var i = 0; i < 8; i++) {
+  var xi = getRandomNumber(0, map.offsetWidth);
+  var yi = getRandomNumber(130, 630);
   var advertData = {
     author: {
       avatar: 'img/avatars/user0' + (i + 1) + '.png'
     },
     offer: {
       title: getRandomItem(OBJECT_TITLES),
-      address: getRandomNumber(0, 1024) + ',' + getRandomNumber(130, 630),
+      address: xi + ', ' + yi,
       price: getRandomNumber(2000, 20000),
       type: getRandomItem(OBJECT_TYPES),
       rooms: getRandomNumber(1, 10),
@@ -57,16 +64,13 @@ for (var i = 0; i < 8; i++) {
       photos: getRandomItem(OBJECT_PHOTOS)
     },
     location: {
-      x: getRandomNumber(0, 1024),
-      y: getRandomNumber(130, 630)
+      x: xi,
+      y: yi
     }
   };
 
   advertsData.push(advertData);
 }
-
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
 
 var pinList = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
@@ -79,8 +83,8 @@ var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pi
 var renderPin = function (pinData) {
   var pinElement = pinTemplate.cloneNode(true);
 
-  pinElement.style.left = pinData.location.x - 25 + 'px';
-  pinElement.style.top = pinData.location.y - 70 + 'px';
+  pinElement.style.left = pinData.location.x + 0.5 * PIN_WIDTH + 'px';
+  pinElement.style.top = pinData.location.y + PIN_HEIGHT + 'px';
   pinElement.querySelector('img').src = pinData.author.avatar;
   pinElement.querySelector('img').alt = pinData.offer.title;
   return pinElement;
